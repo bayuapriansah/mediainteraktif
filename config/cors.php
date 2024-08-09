6c8a1986f260609407ce4af4b12c250cca3fa8f3
bayuapriansah <bayuapriansah10@gmail.com>
2024-08-09 22:16:58 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -15,19 +15,35 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    // 'paths' => ['*'],
 
-    'allowed_methods' => ['*'],
+    // 'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+
+    // 'allowed_origins_patterns' => [],
+
+    // 'allowed_headers' => ['*'],
+
+    // 'exposed_headers' => [],
+
+    // 'max_age' => 0,
+
+    // 'supports_credentials' => true,
+
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+
+    'allowed_origins' => ['https://yourfrontenddomain.com'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['X-Total-Count'],
 
-    'max_age' => 0,
+    'max_age' => 86400, // 24 hours
 
     'supports_credentials' => true,
 
