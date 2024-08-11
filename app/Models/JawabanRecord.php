6c43e654b0306285084b41e2818d0f9cfc80f594
bayuapriansah <bayuapriansah10@gmail.com>
2024-08-11 22:17:13 +0700

    fix models

--- app/Models/JawabanRecord.php
+++ app/Models/JawabanRecord.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class JawabanRecord extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'tugas_materi_id',  // Add this line
+        'user_id',
+        'answer',
+        'score',
+    ];
 }
