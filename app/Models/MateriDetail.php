c7373cbb060c68a823bc52fcdecc20ca96797d25
bayuapriansah <bayuapriansah10@gmail.com>
2024-08-20 00:09:56 +0700

    same for materi details

--- app/Models/MateriDetail.php
+++ app/Models/MateriDetail.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class MateriDetail extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'materi_name_id', // Add this line
+        'materi_1',
+        'image_materi_1',
+        'materi_2',
+        'image_materi_2',
+        // other fields...
+    ];
 }
